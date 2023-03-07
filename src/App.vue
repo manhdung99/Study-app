44d3d693e3f96a8423524595d31b91f91ee8ed75
Nguyen Manh Dung <nguyenmanhdung@sutunam.com>
2023-03-07 19:13:17 +0700

    create vue app with ant design

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from './components/header/Header.vue';
+import Content from './components/content/Content.vue';
+import Navbar from './components/navbar/Navbar.vue';
+</script>
+
+<template>
+  <header>
+    <div>
+      <Header />
+    </div>
+  </header>
+  <main class="flex">
+    <div class="w-1/5">
+    <Navbar />
+    </div>
+    <div>
+    <Content />
+    </div>
+  </main>
+</template>
+
+
